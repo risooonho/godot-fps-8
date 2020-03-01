c423445569e118a6c12d57cc535c82987745a364
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-03-01 17:24:21 -0300

    Refactored player script, added bullet scene, added gun to player, added ability to shoot bullets to player

---
+++ scripts/Bullet.gd
@@ -0,0 +1,24 @@
+extends Area
+
+var damage = 30
+var speed = 20
+
+onready var lifeTime = $LifeTime
+
+func _ready():
+#	connect("body_entered", self, "_on_bullet_body_entered")
+	lifeTime.connect("timeout", self, "_on_lifeTime_timeout")
+	
+func _process(delta):
+	translation += global_transform.basis.x * delta * speed
+
+func _on_bullet_body_entered(body):
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+		destroy()
+		
+func _on_lifeTime_timeout():
+	destroy()
+	
+func destroy():
+	queue_free()
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody
 var speed = 5
 var acceleration = 20
 var gravity = 9.8
-var jump = 5
+var jumpHeight = 5
 
 var mouseSensitivity = 0.2
 
@@ -12,23 +12,36 @@ var velocity = Vector3()
 var fall = Vector3()
 
 onready var head = $Head
+onready var shootPosition = $Head/Camera/Gun/ShootPosition
+onready var bulletScn = preload("res://scenes/Bullet.tscn")
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	lock_mouse()
 
 func _process(delta):
 	direction = Vector3()
+	quit_game()
+	apply_gravity(delta)
+	jump()
+	move()
+	shoot()
+	apply_movement(delta)
 	
-	if not(is_on_floor()):
-		fall.y -= gravity * delta
+func _input(event):
+	if event is InputEventMouseMotion:
+		aim(event)
+		
+func apply_movement(delta):
+	direction = direction.normalized()
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
+	move_and_slide(fall, Vector3.UP)
 	
-	if Input.is_action_just_pressed("jump"):
-		fall.y = jump
-	
-	if Input.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
+func lock_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func move():
 	if Input.is_action_pressed("move_forward"):
 		direction -= transform.basis.z	
 	elif Input.is_action_pressed("move_backward"):
@@ -38,15 +51,27 @@ func _process(delta):
 		direction -= transform.basis.x	
 	elif Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
-		
-	direction = direction.normalized()
-	
-	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
-	velocity = move_and_slide(velocity, Vector3.UP)
-	move_and_slide(fall, Vector3.UP)
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(deg2rad(-event.relative.x * mouseSensitivity))
-		head.rotate_x(deg2rad(-event.relative.y * (mouseSensitivity / 1.5 )))
-		head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
+func quit_game():
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		fall.y -= gravity * delta
+
+func jump():
+	if Input.is_action_just_pressed("jumpHeight"):
+		fall.y = jumpHeight
+
+func aim(mouseEvent: InputEventMouseMotion):
+	rotate_y(deg2rad(-mouseEvent.relative.x * mouseSensitivity))
+	head.rotate_x(deg2rad(-mouseEvent.relative.y * (mouseSensitivity / 1.5 )))
+	head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
+
+func shoot():
+	if Input.is_action_pressed("shoot"):
+		var bullet = bulletScn.instance()
+		bullet.global_transform = shootPosition.global_transform
+		bullet.scale = Vector3.ONE
+		get_node("/root/Game").add_child(bullet)
